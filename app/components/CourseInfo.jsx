69a3c084cf9dd89c063a1065f11f7dd5b623166a
Arvy Gill <arvy307@gmail.com>
2016-08-23 14:08:00 -0700

    More UI Improvements

--- app/components/CourseInfo.jsx
+++ app/components/CourseInfo.jsx
@@ -17,27 +17,26 @@ export default class CourseInfo extends React.Component {
     render() {
         return (
             <div className="course-info-container group">
-                <div className="course-info-title-container">
-                    <div className="course-info-title-left">
-                        {this.props.activeCourse.name}
+                <div className="course-info-header">
+                    {this.props.activeCourse.name}
+                </div>
+                <div style={{padding: 10}}>
+                    <div>
+                        <span>{`${this.props.activeCourse.author}`}</span>
                     </div>
-                    <div className="course-info-title-right">
-                        <div className="course-info-title-right-half">
-                            <span>{this.props.activeCourse.days.map(day => day.substring(0, 3)).join(", ")}</span>
-                        </div>
-                        <div className="course-info-title-right-half">
-                            <span>{this.props.activeCourse.time.join(", ")}</span>
-                        </div>
+                    <div>
+                        <span>{this.props.activeCourse.days.map(day => day.substring(0, 3)).join(", ")}</span> | <span>{this.props.activeCourse.time.join(", ")}</span>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div className="course-info-footer">
+                        <button className="cs-button" onClick={this.handleRemoveFromCalendar}>
+                            <i className="fa fa-calendar-minus-o"></i> Remove from Calendar
+                        </button>
                     </div>
                 </div>
-                <div>
-                    <span>{`${this.props.activeCourse.author}`}</span>
-                </div>
-                <div className="course-info-footer">
-                    <button className="cs-button" onClick={this.handleRemoveFromCalendar}>
-                        <i className="fa fa-calendar-minus-o"></i> Remove from Calendar
-                    </button>
-                </div>
+
             </div>
         );
     }
